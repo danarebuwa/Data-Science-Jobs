44d89f727b47186a056a327daf89454d602a429c
danarebuwa <92400882+danarebuwa@users.noreply.github.com>
2023-06-26 12:15:02 +0100

    Update data_jobs.py

--- data_jobs.py
+++ data_jobs.py
@@ -4,6 +4,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+import plotly
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.neighbors import NearestNeighbors
 
